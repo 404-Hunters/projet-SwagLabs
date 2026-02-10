8675b0427fbe5b72b9c2dd1fb75e49f4b07cbd53
Quentin Plet <quentin.plet@gmail.com>
2026-02-10 15:54:37 +0100

    feat: :sparkles: Creation de la branch develop

--- features/steps/connexion_steps.py
+++
@@ -1,6 +0,0 @@
-from behave import given, when, then
-
-# ===== Scenario: Connexion utilisateur =====
-@given("un utilisateur connecté")
-def step_given_utilisateur_connecte(context):
-    context.utilisateur_connecte = True
